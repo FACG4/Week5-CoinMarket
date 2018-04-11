14277c09f1369e8c4ab160c1111af9a13f87d7da
Mohammed Yehia <m-ya@windowslive.com>
2018-04-11 19:25:51 +0300

    adding final files

--- src/handler.js
+++ src/handler.js
@@ -12,17 +12,18 @@ const contentType = {
 }
 
 const serveFiles = (endpoint, res)=>{
-  const filePath = path.join(__dirname, '..','public', endpoint);//end / /css/style.css /js/dom.js
-  const fileExtention = endpoint.split('.')[1];// css js html
+  const filePath = path.join(__dirname, '..','public', endpoint);
+  const fileExtention = endpoint.split('.')[1];
   res.writeHead(200,{'Content-Type':`${contentType[fileExtention]}`});
   fs.readFile(filePath, (error, file)=>{
-    if (error){console.log(error);
+    if (error){
+      console.log(error);
     }
     res.end(file);
   });
 }
 // let data ;
-const getCurrency = (url , cb)=>{
+const getData = (url , cb)=>{
   http.get(url,response=>{
     response.setEncoding("utf-8");
     let data = '';
@@ -35,14 +36,6 @@ const getCurrency = (url , cb)=>{
     })
   })
 }
-//
-// const request = (url)=>{
-//   getCurrency(url,(err,response,body)=>{
-//      // console.log(body);
-//      data = body
-//   })
-//   return data
-// }
 
 
-module.exports={serveFiles,getCurrency}
+module.exports={serveFiles,getData}
--- src/router.js
+++ src/router.js
@@ -3,21 +3,51 @@ const https = require('https')
 
 const router = (req, res)=>{
   const endpoint = req.url ;
+
   if(endpoint === '/'){
+
       handler.serveFiles('/index.html',res)
 
 
   }else if(endpoint === '/data'){
 
-    handler.getCurrency('https://api.coinmarketcap.com/v1/ticker/?limit=10',(data)=>{
+    handler.getData('https://api.coinmarketcap.com/v1/ticker/?limit=10',(data)=>{
 
       res.end(data)
     });
 
-  } else if(endpoint === "/search"){
+
+  } else if(endpoint === "/convert"){
+      handler.serveFiles('/convert.html',res)
+
+  }else if(endpoint === "/c"){
+    let allData = '' ;
+    req.on("data",(chunk)=>{
+    allData += chunk  ;
+  }).on('end',()=>{
+
+      let splitedData = allData.split(".")
+      handler.getData(`https://min-api.cryptocompare.com/data/price?fsym=${splitedData[0]}&tsyms=${splitedData[1]}`,(data)=>{
+      res.end(data);
+
+    });
+  })
 
 
-  }else {
+}else if(endpoint === "/search"){
+    let allData = '' ;
+    req.on("data",(chunk)=>{
+    allData += chunk  ;
+  }).on('end',()=>{
+
+      handler.getData(`https://api.coinmarketcap.com/v1/ticker/${allData}/`,(data)=>{
+      res.end(data);
+    });
+  })
+
+
+  }
+    else {
     handler.serveFiles(endpoint, res)
   }
 }
